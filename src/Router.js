92d12a37d1fcd5e4dd026b45d7a19ad406056222
rvreshmavijay <reshmavijay21@gmail.com>
2023-11-16 12:49:08 +0530

    reshma hosp login

---
+++ src/Router.js
@@ -0,0 +1,20 @@
+import { BrowserRouter,Routes,Route} from "react-router-dom";
+import React from 'react'
+import './Hospital_log'
+import Hospital_log from "./Hospital_log";
+
+function Router() {
+  return (
+   
+    <div>
+         <BrowserRouter>
+        <Routes>
+        <Route path="/Hospitallogin" element={<Hospital_log/>}/>
+        </Routes>
+        </BrowserRouter>
+    </div>
+    
+  )
+}
+
+export default Router
