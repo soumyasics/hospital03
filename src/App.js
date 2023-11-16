d825f1e9a9c2e4828b8b99492da6b9dbb7b25fb2
vaishNir <vaishnavinairgs@gmail.com>
2023-11-16 13:03:47 +0530

    app form

--- src/App.js
+++ src/App.js
@@ -1,10 +1,12 @@
 import logo from './logo.svg';
 import './App.css';
-
+import AppointmentForm from './AppointmentForm';
+import Router from './Router';
 function App() {
   return (
     <div className="App">
-      
+    <Router></Router>
+    
     </div>
   );
 }
