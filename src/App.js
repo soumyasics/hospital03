31835e6456f2f95267fae9c5238850014d63a860
rvreshmavijay <reshmavijay21@gmail.com>
2023-11-17 11:35:55 +0530

    managementlog

--- src/App.js
+++ src/App.js
@@ -1,12 +1,16 @@
 import { BrowserRouter, Routes, Route } from 'react-router-dom';
 import Hospital_log from './Hospital_log';
-import 
+import StaffLogin from './staff/staff login/StaffLogin';
+import AppointmentForm from './AppointmentForm';
+
 function App() {
   return (
     <div className="App">
     <BrowserRouter>
     <Routes>
     <Route element={<Hospital_log/>} path="/Hospitallogin" />
+    <Route element={<StaffLogin/>} path="/StaffLogin" />
+    <Route element={<AppointmentForm/>} path="/AppointmentForm" />
     </Routes>
     </BrowserRouter>
 
