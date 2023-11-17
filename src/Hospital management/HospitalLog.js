4c7401822d8c74aa99d706f1596ec70488554f49
rvreshmavijay <reshmavijay21@gmail.com>
2023-11-17 12:56:10 +0530

    hsptlogcss

--- src/Hospital management/HospitalLog.js
+++ src/Hospital management/HospitalLog.js
@@ -3,31 +3,31 @@ import './HospitalLog.css';
 
 function HospitalLog() {
   return (
-    <div className="Body">
-    <div className="maindiv">
+    <div className="hospital-login-Body">
+    <div className="hospital-login-maindiv">
       <form>
-        <h1 className="Ul">User Login</h1>
+        <h1 className="hospital-login-Ul">User Login</h1>
 
-  <div className='userpswd'>
+  <div className='hospital-login-userpswd'>
           <input
-            className="userpswd_box"
+            className="hospital-login-userpswd_box"
             type="text"
             placeholder="Username"
           /><br />
           <input
-            className="userpswd_box"
+            className="hospital-login-userpswd_box"
             type="password"
             placeholder="Password"
           />
   </div>
 
-  <div className="Rememberdiv">
+  <div className="hospital-login-Rememberdiv">
           <input className="chkbox" type="checkbox" />
             Remember me
           {/* <a className="FRlink">Forgot password?</a> */}
   </div>
-        <button class="Loginbtn">Login</button>
-        <div className="Register">
+        <button class="hospital-login-Loginbtn">Login</button>
+        <div className="hospital-login-Register">
           <p>
             {/* Not a member?<a className='FRlink'>Register here!</a> */}
           </p>
